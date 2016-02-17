b7914507d242e91f55d92a2d42ffa87ab6941156
esgvaldez <esg.valdez@gmail.com>
2016-02-18 07:20:45 +0800

    recommit

--- AZlabs/tpl/az-content-end.php
+++
@@ -1,6 +0,0 @@
-
-	</div>
-</div>
-<div id='dynaScriptContainer'>
-	<!-- <script src="/wp-content/plugins/AZlabs/assets/js/azlabs.inbound.shipment.plan.js?ts=qwerty1234567890" type="text/javascript"></script> -->
-</div>
